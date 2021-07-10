04acd727449ac9a75567f4b381b26c74025f3ad3
Pro-Abhinav <abhinav16032002@gmail.com>
2021-07-10 21:29:55 +0530

    [ADD] added packages

--- server/index.js
+++ server/index.js
@@ -4,14 +4,16 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
 dotenv.config();
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(cors());
-
-
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`)))
