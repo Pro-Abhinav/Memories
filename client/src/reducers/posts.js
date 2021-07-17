2cdf15ecb02cc773247c644e1be6e4674edc484b
Pro-Abhinav <abhinav16032002@gmail.com>
2021-07-17 21:51:56 +0530

    [ADD] added new features

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,5 +1,11 @@
 export default (posts=[], action) => {
     switch (action.type) {
+        case 'DELETE':
+            return posts.filter((post) => post._id != action.payload);
+        case 'UPDATE':
+        case 'LIKE':
+            console.log(action.payload, "insideUpdateReducer")
+            return posts.map((post) => post._id == action.payload._id ? action.payload : post);
         case 'FETCH_ALL':
             console.log(action.payload, "insideReducers");
             return action.payload;
